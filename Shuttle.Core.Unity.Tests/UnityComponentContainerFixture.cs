8360b092b0202916287cdcb74d6086d95ba80430
Eben <me@ebenroux.co.za>
2017-03-11 11:52:27 +0200

    - update package

--- Shuttle.Core.Unity.Tests/UnityComponentContainerFixture.cs
+++ Shuttle.Core.Unity.Tests/UnityComponentContainerFixture.cs
@@ -34,5 +34,23 @@ namespace Shuttle.Core.Unity.Tests
             RegisterTransient(container);
             ResolveTransient(container);
         }
-    }
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_a_multiple_singleton()
+		{
+			var container = new UnityComponentContainer(new UnityContainer());
+
+			RegisterMultipleSingleton(container);
+			ResolveMultipleSingleton(container);
+		}
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_multiple_transient_components()
+		{
+			var container = new UnityComponentContainer(new UnityContainer());
+
+			RegisterMultipleTransient(container);
+			ResolveMultipleTransient(container);
+		}
+	}
 }
