155d6f6305e69bc75333ac05e07d82fab92ad5c7
Eben <me@ebenroux.co.za>
2017-10-20 12:49:37 +0200

    - v8.0.2

--- Shuttle.Core.Unity/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Unity/Properties/AssemblyInfo.cs
@@ -29,11 +29,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Core.Unity for .NET Framework 4.6.2")]
 #endif
 
-[assembly: AssemblyVersion("8.0.1.0")]
+[assembly: AssemblyVersion("8.0.2.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2017")]
 [assembly: AssemblyProduct("Shuttle.Core")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("8.0.1")]
+[assembly: AssemblyInformationalVersion("8.0.2")]
 [assembly: ComVisible(false)]
 
