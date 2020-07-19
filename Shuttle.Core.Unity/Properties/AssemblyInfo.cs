e596ba41bd58a30aa5c75509076a08bf1f008b45
Eben <me@ebenroux.co.za>
2020-07-19 18:58:09 +0200

    - v10.0.8

--- Shuttle.Core.Unity/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Unity/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.7.0")]
-[assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
+[assembly: AssemblyVersion("10.0.8.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
 [assembly: AssemblyProduct("Shuttle.Core.Unity")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.7")]
+[assembly: AssemblyInformationalVersion("10.0.8")]
 [assembly: ComVisible(false)]
